641e283a45c8f64431288af04650741b5f863352
Alexey Rozhkov <alexisrozhkov@gmail.com>
2023-07-17 22:51:31 +0100

    Clean up a bit more

--- dilated_self_attention/dilated_self_attention.py
+++ dilated_self_attention/dilated_self_attention.py
@@ -6,7 +6,7 @@ import torch
 
 def _segment_and_sparsify(
     x: torch.Tensor, ws: List[int], rs: List[int], head_idx: int
-) -> Tuple[torch.Tensor, torch.Tensor]:
+) -> Tuple[int, torch.Tensor]:
     b, n, c = x.size()
 
     x_indices = torch.arange(0, n, dtype=torch.long, device=x.device)[None, :, None]
@@ -26,11 +26,7 @@ def _segment_and_sparsify(
             ] = segment_indices[:, offset::r, :]
             subatt_idx += 1
 
-    sparse_values = torch.gather(x, 1, sparse_indices).view(
-        (b * num_subatt, max_subatt_n, c)
-    )
-
-    return sparse_indices, sparse_values
+    return max_subatt_n, sparse_indices
 
 
 def _mix_outputs(
@@ -45,17 +41,17 @@ def _mix_outputs(
     att_denom_sums = torch.zeros((out_shape[0], out_shape[1]), device=out_device)
     att_denom_sums.scatter_add_(1, a_indices[:, :, 0], a_denoms)
 
-    out = torch.zeros(out_shape, dtype=out_dtype, device=out_device)
-
     # select attention softmax denominator sums for current sparse indices
     sparse_att_denom_sum = torch.gather(att_denom_sums, 1, a_indices[:, :, 0])
 
     # compute alphas
     alphas = torch.divide(a_denoms, sparse_att_denom_sum)[:, :, None]
 
+    out = torch.zeros(out_shape, dtype=out_dtype, device=out_device)
+
     out.scatter_add_(
         1,
-        a_indices[:, :, : out.shape[2]],
+        a_indices[:, :, :out.shape[2]],
         torch.multiply(a_os, alphas),
     )
 
@@ -77,25 +73,34 @@ class DilatedSelfAttention(torch.nn.Module):
         self.rs = rs
         self.head_idx = head_idx
         self.attn = attn_module
+        self.indices = None
+        self.max_subatt_n = None
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        b, n, c = x.size()
+        b, n, in_c = x.size()
 
-        a_indices, batched_values = _segment_and_sparsify(
-            x, self.ws, self.rs, self.head_idx
-        )
+        if self.indices is None:
+            self.max_subatt_n, self.indices = _segment_and_sparsify(
+                x, self.ws, self.rs, self.head_idx
+            )
 
-        # batch values, compute attention in parallel and "unbatch" outputs
-        batched_os, batched_att_denoms = self.attn(batched_values)
+        # extract subsets for each "subattention" from x elements
+        sparse_x = torch.gather(x, 1, self.indices)
 
-        a_denoms = torch.cat(torch.split(batched_att_denoms, b, 0), dim=1)
-        a_os = torch.cat(torch.split(batched_os, b, 0), dim=1)
+        # batch "subattention" values and process in parallel
+        batched_x = sparse_x.view((-1, self.max_subatt_n, in_c))
+        batched_os, batched_att_denoms = self.attn(batched_x)
+
+        # "unbatch" attention outputs
+        out_c = batched_os.shape[-1]
+        sparse_os = batched_os.view((b, -1, out_c))
+        sparse_denoms = batched_att_denoms.view((b, -1))
 
         return _mix_outputs(
-            (b, n, self.attn.out_dim),  # todo: specify out c explicitly
+            (b, n, out_c),
             x.dtype,
             x.device,
-            a_os,
-            a_denoms,
-            a_indices,
+            sparse_os,
+            sparse_denoms,
+            self.indices,
         )
